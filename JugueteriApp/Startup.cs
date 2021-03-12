23cfce67f1dcca943f507fe6290ab5020e6aee11
IngAlbertoDiaz <alberto.diaz.sw@gmail.com>
2021-03-12 02:45:04 -0600

    [ADD] Funcionalidad CORS para permitir peticiones

--- JugueteriApp/Startup.cs
+++ JugueteriApp/Startup.cs
@@ -35,6 +35,11 @@ namespace JugueteriApp
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiTest", Version = "v1" });
             });
+            services.AddCors(options => options.AddPolicy("AllowWebApp",
+                    builder => builder.AllowAnyOrigin().
+                                        AllowAnyHeader()
+                                        .AllowAnyMethod()
+                             ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +57,8 @@ namespace JugueteriApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors("AllowWebApp");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
